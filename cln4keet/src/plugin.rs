fb82a058874ea74afd8f1ae0426d957e707233e2
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-10-27 10:10:14 +0200

    feat: init port to run the server
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- cln4keet/src/plugin.rs
+++ cln4keet/src/plugin.rs
@@ -1,7 +1,9 @@
+use clightningrpc_plugin::commands::json_utils;
 use clightningrpc_plugin::commands::RPCCommand;
 use clightningrpc_plugin::plugin::Plugin;
+use clightningrpc_plugin::types::LogLevel;
 use rest_api::run_rocket;
-use serde_json::Value;
+use serde_json::{from_value, Value};
 
 #[derive(Clone)]
 pub(crate) struct PluginState;
@@ -25,5 +27,22 @@ pub(crate) fn build_plugin() -> Plugin<PluginState> {
             false,
         )
         .register_notification("shutdown", OnShutdown {})
+        .on_init(&|plugin| -> Value {
+            plugin.log(LogLevel::Debug, "Custom init method called");
+            // FIXME: improve the get option plugin API
+            let conf = if let Some(conf_plugin) = &plugin.conf {
+                conf_plugin
+            } else {
+                panic!("this should never happen")
+            };
+            let server_port = conf.options[0].to_owned();
+            let server_port: i32 = from_value(server_port).unwrap();
+            if server_port >= 0 {
+                plugin.log(LogLevel::Info, "running rest server on port {port}");
+                let path = conf.configuration.lightning_dir.as_str();
+                run_rocket(path);
+            }
+            json_utils::init_payload()
+        })
         .clone()
 }
